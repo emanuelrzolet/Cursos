c2d3cd53dd2d32527bac322ca628d7dfca799734
emanuelrzolet <emanuelzolet1@gmail.com>
2023-10-03 15:15:26 -0300

    Create ex009.js

---
+++ Curso JavaScript/aula11/ex008.js
@@ -0,0 +1,8 @@
+console.log('O console funcionou corretamente"!');
+var vel = 60.5;
+if (vel > 80) {
+	console.log("Voce ultrapassou a velocidade permitida. Multado");
+} else {
+	console.log("voce está na velocidade permitida");
+}
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
