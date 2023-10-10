6e494c00347d9c36d2e792f5eb30b651caf8d850
emanuelrzolet <emanuelzolet1@gmail.com>
2023-10-10 16:05:40 -0300

    aula 13

---
+++ Curso JavaScript/aula13/ambiente.js
@@ -0,0 +1,17 @@
+c = 1;
+while (true) {
+	console.log("Teste");
+	c++;
+	if (c == 50) {
+		break;
+	}
+}
+c = 1;
+do {
+	console.log(c);
+	c++;
+} while (c <= 50);
+
+for (c = 1; c < 10; c++) {
+	console.log(c);
+}
