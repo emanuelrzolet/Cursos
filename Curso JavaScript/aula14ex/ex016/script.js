dfdcc703bc257cdc0d080d899ed52f1e31f4fa44
emanuelrzolet <emanuelzolet1@gmail.com>
2023-10-11 10:25:59 -0300

    Update script.js

--- Curso JavaScript/aula14ex/ex016/script.js
+++ Curso JavaScript/aula14ex/ex016/script.js
@@ -4,15 +4,23 @@ function calcular() {
 	let passo = window.document.querySelector("input#passo").value;
 	let res = window.document.querySelector("article#resultado");
 	if (inicio.length == 0 || fim.length == 0 || passo.length == 0) {
+		res.innerHTML = "impossivel contar";
 		window.alert("[Erro] Impossível contar, verifique os campos digitados.");
 	} else {
 		res.innerHTML = `Contando: `;
 		let i = Number(inicio);
 		let f = Number(fim);
 		let p = Number(passo);
-		for (let c = i; c <= f; c += p) {
-			res.innerHTML += `${c} \u{1F449}`;
+		if (i < f) {
+			for (let c = i; c <= f; c += p) {
+				res.innerHTML += `${c} \u{1F449}`;
+			}
+			res.innerHTML += `\u{1F3C1}`;
+		} else {
+			for (let c = i; c >= f; c -= p) {
+				res.innerHTML += `${c} \u{1F449}`;
+			}
 		}
-		res.innerHTML += `\u{1F3C1}`;
+		res.innerHTML += `${c} \u{1F449}`;
 	}
 }
