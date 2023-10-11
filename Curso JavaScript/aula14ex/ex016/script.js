e334b5c44c93f3143af68ef7ddc14125e3387fcb
emanuelrzolet <emanuelzolet1@gmail.com>
2023-10-11 09:35:15 -0300

    ex16

--- Curso JavaScript/aula14ex/ex016/script.js
+++ Curso JavaScript/aula14ex/ex016/script.js
@@ -1,1 +1,19 @@
-alert("Teste");
+function calcular() {
+	var txtinicio = window.document.querySelector("input#inicio").value;
+	var txtfim = window.document.querySelector("input#fim").value;
+	var txtpasso = window.document.querySelector("input#passo").value;
+	inicio = Number(txtinicio);
+	fim = Number(txtfim);
+	passo = Number(txtpasso);
+	var res = window.document.querySelector("article#resultado");
+	res.innerHTML = `Contando: <br>`;
+	res.innerHTML += `${inicio}`;
+	// Bloco nao funcionando
+	if (inicio != null || fim != null || passo != null) {
+		while (inicio + passo < fim) {
+			res.innerHTML += `${inicio}`;
+		}
+	} else {
+		res.innerHTML = "Impossível contar, verifique os campos digitados.";
+	}
+}
