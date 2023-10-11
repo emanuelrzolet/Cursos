bf862302cac75c1e5099d5634e886a948f822ffe
emanuelrzolet <emanuelzolet1@gmail.com>
2023-10-11 13:34:47 -0300

    Finalizado exercicio 17

--- Curso JavaScript/aula14ex/ex017/script.js
+++ Curso JavaScript/aula14ex/ex017/script.js
@@ -1,1 +1,16 @@
-alert("Teste");
+function gerarTabuada() {
+	let num = document.querySelector("input#numero");
+	let tab = document.getElementById("tabuada");
+	if (num.value.length == 0) {
+		window.alert("Digite um número válido!");
+	} else {
+		tab.innerHTML = "";
+		let n = Number(num.value);
+		for (let c = 1; c <= 10; c++) {
+			let item = document.createElement("option");
+			item.text = `${n} x ${c} = ${n * c}`;
+			item.value = `tab${c}`;
+			tab.appendChild(item);
+		}
+	}
+}
