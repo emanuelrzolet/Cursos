c9da1d5045b0a48ce0aefe4041f12c4a19cd2a4f
emanuelrzolet <emanuelzolet1@gmail.com>
2023-10-23 17:07:17 -0300

    End of lecture 3

--- Curso HarvardX CS50W Web Programming/lecture 3/Django/tasks/views.py
+++ Curso HarvardX CS50W Web Programming/lecture 3/Django/tasks/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 
-tasks = ['foo', 'bar', 'baz']
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    priority = forms.IntegerField(label="Priority",min_value=0, max_value=10)
+
+
+tasks = []
+
+
 # Create your views here.
 def index(request):
-    return render(request, "tasks/index.html",{"tasks":tasks})
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html",{"tasks":request.session["tasks"]})
 
 def add(request):
-    return render(request, "task/add.html")
+    if request.method == "POST":
+        forms = NewTaskForm(request.POST)
+        if forms.is_valid():
+            task = forms.cleaned_data["task"]
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+           return render(request, "tasks/add.html",{"forms": forms})
+    return render(request,"tasks/add.html",{"forms": NewTaskForm()})
