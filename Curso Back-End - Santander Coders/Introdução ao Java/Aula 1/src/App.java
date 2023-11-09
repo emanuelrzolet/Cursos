564a29b2686e7297f392aab61038fb99a960f5c0
emanuelrzolet <emanuelzolet1@gmail.com>
2023-11-09 17:11:37 -0300

    Finalizado introdução java

--- Curso Back-End - Santander Coders/Introdução ao Java/Aula 1/src/App.java
+++ Curso Back-End - Santander Coders/Introdução ao Java/Aula 1/src/App.java
@@ -1,6 +1,16 @@
+import java.time.LocalDate;
+import java.util.Arrays;
 public class App {
     public static void main(String[] args) throws Exception {
         String nome = "Emanuel";
+        LocalDate hoje = LocalDate.now();
+        System.out.println(hoje.getDayOfWeek());
         System.out.println("Hello, " + nome.toUpperCase());
+        for (int i=1; i<=10;i++){
+            System.out.println(i);
+        }
+        String[] letras = {"A","B","C","D"};
+        System.out.println(Arrays.toString(letras));
+
     }
 }
