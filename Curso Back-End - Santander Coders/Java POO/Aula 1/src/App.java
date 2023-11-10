8e04780d9c177a44b9e1d34f53cad3ed2cbbd24c
emanuelrzolet <emanuelzolet1@gmail.com>
2023-11-10 15:57:45 -0300

    Criado exercicio

--- Curso Back-End - Santander Coders/Java POO/Aula 1/src/App.java
+++ Curso Back-End - Santander Coders/Java POO/Aula 1/src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner espaçoScanner = new Scanner(System.in);
+        Float espaço;
+        Scanner tempoScanner = new Scanner(System.in);
+        Float tempo;
+
+        System.out.println("Digite o espaço: ");
+        espaço= espaçoScanner.nextFloat();
+        System.out.println("Digite o tempo: ");
+        tempo = espaçoScanner.nextFloat();
+        System.out.println(calcularVM(espaço, tempo));
+
+
+    }
+
+    //Metodo para medir a velocidade media V = s/t
+    public static float calcularVM(float espaço, float tempo){
+        return espaço/tempo;
     }
 }
