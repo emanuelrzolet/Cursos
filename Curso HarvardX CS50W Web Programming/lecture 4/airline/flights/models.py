75177e7b98d46c4a8cb912f1d8855e3b39b2d7f2
emanuelrzolet <emanuelzolet1@gmail.com>
2024-02-05 21:03:24 -0300

    Create Model class passenger

--- Curso HarvardX CS50W Web Programming/lecture 4/airline/flights/models.py
+++ Curso HarvardX CS50W Web Programming/lecture 4/airline/flights/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Airport(models.Model):
     code = models.CharField(max_length=3)
@@ -16,3 +17,13 @@ class Flight(models.Model):
     
     def __str__(self):
         return f"{self.id}: {self.origin} to {self.destination}"
+    
+    
+class Passenger(models.Model):
+    first = models.CharField(max_length=64)
+    last = models.CharField(max_length=64)
+    flights = models.ManyToManyField(Flight, blank=True,related_name="passengers")
+    
+    
+    def __str__(self) -> str:
+        return f"{self.first} {self.last}"
